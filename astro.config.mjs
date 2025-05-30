72d4f96fdfdb83b6492b2cd2c3e3a49bc9559fd4
juanberrio0399 <juandyb99@gmail.com>
2025-05-30 18:02:25 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-import { defineConfig } from '.astro/config';
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  output: 'static',
+  integrations: [tailwind()],
 });
